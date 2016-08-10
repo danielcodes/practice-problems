fef3405d45c317db0bcb095523eb3f2e49c0a497
Daniel Chia <daniel_1118@hotmail.com>
2016-08-10 13:43:28 -0400

    modified to search for first and last occurrence

--- binary_search.py
+++ binary_search.py
@@ -9,11 +9,22 @@ def binarySearch(arr, x):
     low = 0
     high = len(arr) - 1
 
+    result = -1
+
     while low <= high:
         mid = (high + low) / 2
         
         if x == arr[mid]:
-            return mid
+            # return mid
+            result = mid
+
+            # depending on what we want, keep searching left or right
+            # first ocurrence, go left
+            # high = mid - 1 
+
+            # last, go right
+            low = mid + 1
+
         elif x < arr[mid]:
             # if value is less than the middle
             # use lower half by settin ga new high
@@ -23,15 +34,11 @@ def binarySearch(arr, x):
             # use later half by setting a new low
             low = mid + 1
 
-    return -1
+    return result
 
 print a
 print binarySearch(a, 10)
-
-
-
-
-
+print binarySearch(a, 11)
 
 
 
